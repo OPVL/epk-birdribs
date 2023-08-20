cdc3514737dcf85b48055be21b53e6def5849a8c
Lloyd Culpepper <lloydculpepper4@gmail.com>
2023-08-20 19:55:29 +0100

    v0.1

---
+++ index.php
@@ -0,0 +1,263 @@
+<?php
+// Available images:
+// BIRDRIBS_FILM-02.jpg  BIRDRIBS_FILM-13.jpg  BIRDRIBS_FILM-23.jpg  BIRDRIBS_FILM-36.jpg     BIRDRIBS_FISHEYE-17.jpg
+// BIRDRIBS_FILM-03.jpg  BIRDRIBS_FILM-17.jpg  BIRDRIBS_FILM-25.jpg  BIRDRIBS_FISHEYE-05.jpg
+// BIRDRIBS_FILM-07.jpg  BIRDRIBS_FILM-18.jpg  BIRDRIBS_FILM-26.jpg  BIRDRIBS_FISHEYE-06.jpg
+// BIRDRIBS_FILM-09.jpg  BIRDRIBS_FILM-19.jpg  BIRDRIBS_FILM-32.jpg  BIRDRIBS_FISHEYE-11.jpg
+
+// Available videos:
+// https://www.youtube-nocookie.com/embed/qji-m2F3kv0?si=_n8hsg053alvNgJc&amp;start=10
+
+$images = [
+  'BIRDRIBS_FILM-02.webp',
+  'BIRDRIBS_FILM-03.webp',
+  'BIRDRIBS_FILM-07.webp',
+  'BIRDRIBS_FILM-09.webp',
+  'BIRDRIBS_FILM-13.webp',
+  'BIRDRIBS_FILM-17.webp',
+  'BIRDRIBS_FILM-18.webp',
+  'BIRDRIBS_FILM-19.webp',
+  'BIRDRIBS_FILM-23.webp',
+  'BIRDRIBS_FILM-25.webp',
+  'BIRDRIBS_FILM-26.webp',
+  'BIRDRIBS_FILM-32.webp',
+  'BIRDRIBS_FILM-36.webp',
+  'BIRDRIBS_FISHEYE-05.webp',
+  'BIRDRIBS_FISHEYE-06.webp',
+  'BIRDRIBS_FISHEYE-11.webp',
+  'BIRDRIBS_FISHEYE-17.webp',
+];
+
+$creditedImages = [
+  [
+    'file' => 'BIRDRIBS_FILM-02.webp',
+    'title' => 'BIRDRIBS_FILM-02',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-03.webp',
+    'title' => 'BIRDRIBS_FILM-03',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-07.webp',
+    'title' => 'BIRDRIBS_FILM-07',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-09.webp',
+    'title' => 'BIRDRIBS_FILM-09',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-13.webp',
+    'title' => 'BIRDRIBS_FILM-13',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-17.webp',
+    'title' => 'BIRDRIBS_FILM-17',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-18.webp',
+    'title' => 'BIRDRIBS_FILM-18',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-19.webp',
+    'title' => 'BIRDRIBS_FILM-19',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-23.webp',
+    'title' => 'BIRDRIBS_FILM-23',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-25.webp',
+    'title' => 'BIRDRIBS_FILM-25',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-26.webp',
+    'title' => 'BIRDRIBS_FILM-26',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-32.webp',
+    'title' => 'BIRDRIBS_FILM-32',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FILM-36.webp',
+    'title' => 'BIRDRIBS_FILM-36',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FISHEYE-05.webp',
+    'title' => 'BIRDRIBS_FISHEYE-05',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FISHEYE-06.webp',
+    'title' => 'BIRDRIBS_FISHEYE-06',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FISHEYE-11.webp',
+    'title' => 'BIRDRIBS_FISHEYE-11',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS_FISHEYE-17.webp',
+    'title' => 'BIRDRIBS_FISHEYE-17',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+];
+
+shuffle($creditedImages);
+
+$videos = [
+  ['title' => '', 'url' => 'https://www.youtube-nocookie.com/embed/qji-m2F3kv0?si=_n8hsg053alvNgJc&amp;start=10']
+];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>EPK | BIRDRIBS</title>
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <meta name="description" content="BIRDRIBS electronic press kit" />
+  <meta property="og:title" content="EPK | BIRDRIBS" />
+  <meta property="og:description" content="BIRDRIBS electronic press kit" />
+  <meta property="og:image" content="https://birdribs.uk/assets/img/BIRDRIBS_FILM-02.webp" />
+  <meta property="og:url" content="https://birdribs.uk" />
+  <meta property="og:type" content="website" />
+  <meta name=”robots” content="index, follow">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+  <link rel="manifest" href="/assets/site.webmanifest">
+  <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#111111">
+  <link rel="shortcut icon" href="/assets/favicon.ico">
+  <meta name="msapplication-TileColor" content="#111111">
+  <meta name="msapplication-config" content="/assets/browserconfig.xml">
+  <meta name="theme-color" content="#111111">
+</head>
+
+<body>
+  <header class="flex-col">
+    <div class="logo">
+      <h1 class="placeholder">birdribs</h1>
+    </div>
+    <div class="subheading">
+      <h2 class="placeholder">electronic press kit</h2>
+    </div>
+    <div id="featured-images" class="flex-col">
+      <div class="images flex-row scroller">
+        <?php foreach ($creditedImages as $key => $image): ?>
+          <div class="featured-image scrollable">
+            <img src="/assets/img/<?php echo ($image['file']) ?>" alt="<?php echo ($image['title']) ?>" <?php echo ($key > 5 ? 'loading="lazy"' : '') ?> />
+            <p>
+              <a href="<?php echo ($image['url']) ?>" target="_blank" rel="noreferrer noopener">
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                <?php echo ($image['overlay-text']) ?>
+              </a>
+            </p>
+          </div>
+        <?php endforeach; ?>
+      </div>
+      <a class=" download-button"
+        href="https://drive.google.com/drive/folders/1j-LRzL3qPm0efdfrsvx2DiMQtaVG1SLK?usp=sharing"
+        aria-label="high resolution images (opens in new tab)" target="_blank" rel="noopener noreferrer">
+        <i class="fa-solid fa-cloud-arrow-down"></i> high res originals</a>
+    </div>
+  </header>
+  <main>
+    <div class="flex-row about-section">
+      <section class="bio flex-col">
+        <h2 class="placeholder">bio</h2>
+        <p class="placeholder">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem iste sed in libero asperiores iure deserunt beatae
+          quis corrupti sunt! Corrupti laborum, delectus optio enim ex minima
+          doloribus quas. Tempore!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+          velit, cum amet facilis deleniti tempore doloremque eaque illum
+          distinctio laboriosam, ea recusandae, ab error. Doloremque provident
+          quasi deserunt ipsum maiores.
+        </p>
+      </section>
+      <section class="music flex-col">
+        <h2 class="title">music</h2>
+        <div class="embed flex-col">
+          <iframe title="bandcamp embed" style="border: 0; width: 500px; height: 672px"
+            src="https://bandcamp.com/EmbeddedPlayer/album=1243833148/size=large/bgcol=333333/linkcol=ffffff/transparent=true/"
+            seamless><a href="https://birdribs.bandcamp.com/album/sleep-clinic-redundant-ave">Sleep Clinic // Redundant
+              Ave by BIRDRIBS</a></iframe>
+        </div>
+      </section>
+    </div>
+    <div class="flex-row about-section">
+      <section class="videos flex-col">
+        <h2 class="title">videos</h2>
+        <div class="flex-row scroller">
+          <div class="scrollable flex-col">
+            <iframe width="560" height="315"
+              src="https://www.youtube-nocookie.com/embed/qji-m2F3kv0?si=_n8hsg053alvNgJc&amp;start=10"
+              title="birdribs @ vinestock 2023" frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen></iframe>
+            <p>live @ vinestock 2023</p>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="flex-col about-section">
+      <section class="contact flex-col">
+        <h2 class="title">all enquiries</h2>
+        <div class="flex-row">
+          <div class="contact-details flex-col">
+            <p>
+              <a class="download-button" href="mailto:bookings@birdribs.uk" aria-label="email bookings" target="_blank"
+                rel="noopener noreferrer">
+                <i class="fa-solid fa-envelope"></i>
+                bookings@birdribs.uk
+              </a>
+            </p>
+          </div>
+        </div>
+    </div>
+  </main>
+  <footer>
+    <div class="copyright">
+      <a href="https://lloydculpepper.uk" rel="noopener noreferrer">© 2023 opvl</a>
+    </div>
+  </footer>
+  <script src="https://kit.fontawesome.com/d83d45983d.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
