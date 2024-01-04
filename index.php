e095be0db6bc6344308ece12bdd29576e16126a7
opvl <ahoy@lloydculpepper.uk>
2024-01-04 15:12:07 +0000

    updated content

--- index.php
+++ index.php
@@ -1,19 +1,53 @@
 <?php
 // Available images:
-// BIRDRIBS_FILM-02.jpg  BIRDRIBS_FILM-13.jpg  BIRDRIBS_FILM-23.jpg  BIRDRIBS_FILM-36.jpg     BIRDRIBS_FISHEYE-17.jpg
-// BIRDRIBS_FILM-03.jpg  BIRDRIBS_FILM-17.jpg  BIRDRIBS_FILM-25.jpg  BIRDRIBS_FISHEYE-05.jpg
-// BIRDRIBS_FILM-07.jpg  BIRDRIBS_FILM-18.jpg  BIRDRIBS_FILM-26.jpg  BIRDRIBS_FISHEYE-06.jpg
-// BIRDRIBS_FILM-09.jpg  BIRDRIBS_FILM-19.jpg  BIRDRIBS_FILM-32.jpg  BIRDRIBS_FISHEYE-11.jpg
+// 000011290004.jpg   BIRDRIBS-40.jpg   BIRDRIBS-85.jpg         BIRDRIBS_FILM-25.webp      RIBS-19.jpg
+// 000011290005.jpg   BIRDRIBS-44.jpg   BIRDRIBS-86.jpg         BIRDRIBS_FILM-26.webp      RIBS-23.jpg
+// 000011290006.jpg   BIRDRIBS-52.jpg   BIRDRIBS-88.jpg         BIRDRIBS_FILM-32.webp      RIBS-27.jpg
+// 000011290020.jpg   BIRDRIBS-54.jpg   BIRDRIBS_FILM-02.webp   BIRDRIBS_FILM-36.webp      RIBS-28.jpg
+// 000011290021.jpg   BIRDRIBS-55.jpg   BIRDRIBS_FILM-03.webp   BIRDRIBS_FISHEYE-05.webp   RIBS-31.jpg
+// BIRDRIBS-02.jpg    BIRDRIBS-58.jpg   BIRDRIBS_FILM-07.webp   BIRDRIBS_FISHEYE-06.webp   RIBS-35.jpg
+// BIRDRIBS-04.jpg    BIRDRIBS-61.jpg   BIRDRIBS_FILM-09.webp   BIRDRIBS_FISHEYE-11.webp   RIBS-36.jpg
+// BIRDRIBS-06.jpg    BIRDRIBS-66.jpg   BIRDRIBS_FISHEYE-17.webpRIBS-41.jpg
+// BIRDRIBS-13.jpg    BIRDRIBS-72.jpg   BIRDRIBS_FILM-17.webp   RIBS-06.jpg                RIBS-45.jpg
+// BIRDRIBS-16.jpg    BIRDRIBS-74.jpg   BIRDRIBS_FILM-18.webp  'RIBS-08 (1).jpg'
+// BIRDRIBS-25.jpg    BIRDRIBS-78.jpg   BIRDRIBS_FILM-19.webp   RIBS-13.jpg
+// BIRDRIBS-35.jpg    BIRDRIBS-84.jpg   BIRDRIBS_FILM-23.webp   RIBS-15.jpg
 
 // Available videos:
 // https://www.youtube-nocookie.com/embed/qji-m2F3kv0?si=_n8hsg053alvNgJc&amp;start=10
 
-$images = [
+$available_images = [
+  '000011290004.jpg',
+  '000011290005.jpg',
+  '000011290006.jpg',
+  '000011290020.jpg',
+  '000011290021.jpg',
+  'BIRDRIBS-02.jpg',
+  'BIRDRIBS-04.jpg',
+  'BIRDRIBS-06.jpg',
+  'BIRDRIBS-13.jpg',
+  'BIRDRIBS-16.jpg',
+  'BIRDRIBS-25.jpg',
+  'BIRDRIBS-35.jpg',
+  'BIRDRIBS-40.jpg',
+  'BIRDRIBS-44.jpg',
+  'BIRDRIBS-52.jpg',
+  'BIRDRIBS-54.jpg',
+  'BIRDRIBS-55.jpg',
+  'BIRDRIBS-58.jpg',
+  'BIRDRIBS-61.jpg',
+  'BIRDRIBS-66.jpg',
+  'BIRDRIBS-72.jpg',
+  'BIRDRIBS-74.jpg',
+  'BIRDRIBS-78.jpg',
+  'BIRDRIBS-84.jpg',
+  'BIRDRIBS-85.jpg',
+  'BIRDRIBS-86.jpg',
+  'BIRDRIBS-88.jpg',
   'BIRDRIBS_FILM-02.webp',
   'BIRDRIBS_FILM-03.webp',
   'BIRDRIBS_FILM-07.webp',
   'BIRDRIBS_FILM-09.webp',
-  'BIRDRIBS_FILM-13.webp',
   'BIRDRIBS_FILM-17.webp',
   'BIRDRIBS_FILM-18.webp',
   'BIRDRIBS_FILM-19.webp',
@@ -26,9 +60,194 @@ $images = [
   'BIRDRIBS_FISHEYE-06.webp',
   'BIRDRIBS_FISHEYE-11.webp',
   'BIRDRIBS_FISHEYE-17.webp',
+  'RIBS-06.jpg',
+  'RIBS-08 (1).jpg',
+  'RIBS-13.jpg',
+  'RIBS-15.jpg',
+  'RIBS-19.jpg',
+  'RIBS-23.jpg',
+  'RIBS-27.jpg',
+  'RIBS-28.jpg',
+  'RIBS-31.jpg',
+  'RIBS-35.jpg',
+  'RIBS-36.jpg',
+  'RIBS-41.jpg',
+  'RIBS-45.jpg'
 ];
 
+// $creditedImages
+// this is the array that will be used to display the images, it will be populated with the images that have been credited
+// example schema:
+// [
+//   'file' => 'BIRDRIBS_FILM-02.webp', // this is the filename of the image
+//   'title' => 'BIRDRIBS_FILM-02', // this is the title of the image, it's the filename without the extension
+//   'overlay-text' => 'Steven Tatlow', // this is always set to Steven Tatlow, but it's here for completeness
+//   'url' => 'https://www.instagram.com/stevetatlow/' // this is always set to https://www.instagram.com/stevetatlow/, but it's here for completeness
+// ],
+
 $creditedImages = [
+  [
+    'file' => '000011290004-min.jpg',
+    'title' => '000011290004',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => '000011290005-min.jpg',
+    'title' => '000011290005',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => '000011290006-min.jpg',
+    'title' => '000011290006',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => '000011290020-min.jpg',
+    'title' => '000011290020',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => '000011290021-min.jpg',
+    'title' => '000011290021',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-02-min.jpg',
+    'title' => 'BIRDRIBS-02',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-04-min.jpg',
+    'title' => 'BIRDRIBS-04',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-06-min.jpg',
+    'title' => 'BIRDRIBS-06',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-13-min.jpg',
+    'title' => 'BIRDRIBS-13',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-16-min.jpg',
+    'title' => 'BIRDRIBS-16',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-25-min.jpg',
+    'title' => 'BIRDRIBS-25',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-35-min.jpg',
+    'title' => 'BIRDRIBS-35',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-40-min.jpg',
+    'title' => 'BIRDRIBS-40',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-44-min.jpg',
+    'title' => 'BIRDRIBS-44',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-52-min.jpg',
+    'title' => 'BIRDRIBS-52',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-54-min.jpg',
+    'title' => 'BIRDRIBS-54',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-55-min.jpg',
+    'title' => 'BIRDRIBS-55',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-58-min.jpg',
+    'title' => 'BIRDRIBS-58',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-61-min.jpg',
+    'title' => 'BIRDRIBS-61',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-66-min.jpg',
+    'title' => 'BIRDRIBS-66',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-72-min.jpg',
+    'title' => 'BIRDRIBS-72',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-74-min.jpg',
+    'title' => 'BIRDRIBS-74',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-78-min.jpg',
+    'title' => 'BIRDRIBS-78',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-84-min.jpg',
+    'title' => 'BIRDRIBS-84',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-85-min.jpg',
+    'title' => 'BIRDRIBS-85',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-86-min.jpg',
+    'title' => 'BIRDRIBS-86',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'BIRDRIBS-88-min.jpg',
+    'title' => 'BIRDRIBS-88',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
   [
     'file' => 'BIRDRIBS_FILM-02.webp',
     'title' => 'BIRDRIBS_FILM-02',
@@ -50,12 +269,6 @@ $creditedImages = [
   [
     'file' => 'BIRDRIBS_FILM-09.webp',
     'title' => 'BIRDRIBS_FILM-09',
-    'overlay-text' => 'Steven Tatlow',
-    'url' => 'https://www.instagram.com/stevetatlow/'
-  ],
-  [
-    'file' => 'BIRDRIBS_FILM-13.webp',
-    'title' => 'BIRDRIBS_FILM-13',
     'overlay-text' => 'Steven Tatlow',
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
@@ -108,32 +321,89 @@ $creditedImages = [
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
   [
-    'file' => 'BIRDRIBS_FISHEYE-05.webp',
-    'title' => 'BIRDRIBS_FISHEYE-05',
+    'file' => 'RIBS-06-min.jpg',
+    'title' => 'RIBS-06',
     'overlay-text' => 'Steven Tatlow',
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
   [
-    'file' => 'BIRDRIBS_FISHEYE-06.webp',
-    'title' => 'BIRDRIBS_FISHEYE-06',
+    'file' => 'RIBS-08 (1)-min.jpg',
+    'title' => 'RIBS-08 (1)',
     'overlay-text' => 'Steven Tatlow',
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
   [
-    'file' => 'BIRDRIBS_FISHEYE-11.webp',
-    'title' => 'BIRDRIBS_FISHEYE-11',
+    'file' => 'RIBS-13-min.jpg',
+    'title' => 'RIBS-13',
     'overlay-text' => 'Steven Tatlow',
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
   [
-    'file' => 'BIRDRIBS_FISHEYE-17.webp',
-    'title' => 'BIRDRIBS_FISHEYE-17',
+    'file' => 'RIBS-15-min.jpg',
+    'title' => 'RIBS-15',
     'overlay-text' => 'Steven Tatlow',
     'url' => 'https://www.instagram.com/stevetatlow/'
   ],
+  [
+    'file' => 'RIBS-19-min.jpg',
+    'title' => 'RIBS-19',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-23-min.jpg',
+    'title' => 'RIBS-23',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-27-min.jpg',
+    'title' => 'RIBS-27',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-28-min.jpg',
+    'title' => 'RIBS-28',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-31-min.jpg',
+    'title' => 'RIBS-31',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-35-min.jpg',
+    'title' => 'RIBS-35',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-36-min.jpg',
+    'title' => 'RIBS-36',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-41-min.jpg',
+    'title' => 'RIBS-41',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ],
+  [
+    'file' => 'RIBS-45-min.jpg',
+    'title' => 'RIBS-45',
+    'overlay-text' => 'Steven Tatlow',
+    'url' => 'https://www.instagram.com/stevetatlow/'
+  ]
 ];
 
+// shuffle images so there's no arguments about who's image is first
+// we then slice the array to only include the first 40 images
 shuffle($creditedImages);
+// $creditedImages = array_slice($creditedImages, 0, 40);
 
 $videos = [
   ['title' => '', 'url' => 'https://www.youtube-nocookie.com/embed/qji-m2F3kv0?si=_n8hsg053alvNgJc&amp;start=10']
@@ -141,9 +411,10 @@ $videos = [
 
 // shuffle credits so there's no arguments about who's name is first
 $artists = [
-  ['name' => 'sean walker', 'role' => 'vocals & guitar'],
+  ['name' => 'sean walker', 'role' => 'vocals/guitar'],
   ['name' => 'lloyd culpepper', 'role' => 'bass'],
-  ['name' => 'george carey', 'role' => 'drums']
+  ['name' => 'george carey', 'role' => 'drums'],
+  ['name' => 'tom ponting', 'role' => 'guitar']
 ];
 shuffle($artists);
 ?>
@@ -238,8 +509,8 @@ shuffle($artists);
       <section class="music flex-col">
         <h2 class="title">music</h2>
         <div class="embed flex-col">
-          <iframe style="border: 0; width: 360px; height: 532px;" src="https://bandcamp.com/EmbeddedPlayer/album=1243833148/size=large/bgcol=333333/linkcol=ffffff/transparent=true/" seamless><a href="https://birdribs.bandcamp.com/album/sleep-clinic-redundant-ave">Sleep Clinic // Redundant
-              Ave by BIRDRIBS</a></iframe>
+          <iframe style="border: 0; width: 350px; height: 786px;" src="https://bandcamp.com/EmbeddedPlayer/album=2813569206/size=large/bgcol=333333/linkcol=0f91ff/transparent=true/" seamless><a href="https://birdribs.bandcamp.com/album/at-the-sweaty-jazz-cafe">at the sweaty jazz cafe by BIRDRIBS</a></iframe>
+          <iframe style="border: 0; width: 350px; height: 522px;" src="https://bandcamp.com/EmbeddedPlayer/album=1243833148/size=large/bgcol=333333/linkcol=0f91ff/transparent=true/" seamless><a href="https://birdribs.bandcamp.com/album/sleep-clinic-redundant-ave">Sleep Clinic // Redundant Ave by BIRDRIBS</a></iframe>
         </div>
       </section>
     </div>
